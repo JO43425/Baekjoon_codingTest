bd158142d46ba6c79717c9515de8990c2f8b05a5
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-06-22 00:48:15 +0900

    Clear Main

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,35 +1,10 @@
 ﻿// CodingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-#include <stdio.h>
-#include <vector>
-struct Data
-{
-	int A, B;
-};
-
-int main(void)
-{
-	int n;
-	std::vector<Data> datas;
-	scanf("%d", &n);
-	datas.reserve(n);
-	for (int i = 0; i < n; ++i)
-	{
-		Data data;
-		scanf("%d %d", &data.A, &data.B);
-		datas.push_back(data);
-	}
-
-	for (int i = 0; i < datas.size(); ++i)
-	{
-		printf("%d\n", datas[i].A + datas[i].B);
-	}
-}
-
 
 /*
-int main()
+#include <stdio.h>
+
+void main(void)
 {
-	return 0;
 }
 */
