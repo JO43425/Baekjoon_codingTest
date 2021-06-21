7c7f3c3e9c916d1edabe2ce890a62abfd5894617
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-06-22 00:54:47 +0900

    Clear Main

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,17 +1,6 @@
 ﻿// CodingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <stdio.h>
-
-int main(void)
-{
-	int n, answer = 0;
-	scanf("%d", &n);
-	for (int i = 1; i <= n; ++i)
-		answer += i;
-	printf("%d", answer);
-}
-
 /*
 #include <stdio.h>
 
