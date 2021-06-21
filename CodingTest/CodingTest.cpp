e7a52b7114226f6cdbd463b6b42d07c68189e1c9
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-06-22 00:41:10 +0900

    Question number 10950
    Enter two integers A and B, then write a program that outputs A+B.

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,13 +1,28 @@
 ﻿// CodingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 #include <stdio.h>
+#include <vector>
+struct Data
+{
+	int A, B;
+};
+
 int main(void)
 {
 	int n;
+	std::vector<Data> datas;
 	scanf("%d", &n);
-	for (int i = 1; i < 10; ++i)
+	datas.reserve(n);
+	for (int i = 0; i < n; ++i)
 	{
-		printf("%d * %d = %d\n", n, i, n*i);
+		Data data;
+		scanf("%d %d", &data.A, &data.B);
+		datas.push_back(data);
+	}
+
+	for (int i = 0; i < datas.size(); ++i)
+	{
+		printf("%d\n", datas[i].A + datas[i].B);
 	}
 }
 
