7f9551b900d9c094581c4a96512530175bfe21d6
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-10-07 10:11:52 +0900

    C# 코딩 테스트 추가

---
+++ C#/CodingTest/CodingTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    [System.Serializable]
+    public class C
+    {
+        public int a;
+    }
+
+    [System.Serializable]
+    public class B
+    {
+        public C c;
+    }
+
+    [System.Serializable]
+    public class A
+    {
+        public B _b;
+        public C _B
+        { get
+            {
+                return _b?.c;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A { _b = null };
+            C c = a._B;
+
+            if (c == null)
+            {
+                System.Console.WriteLine("null");
+            }
+        }
+    }
+}
