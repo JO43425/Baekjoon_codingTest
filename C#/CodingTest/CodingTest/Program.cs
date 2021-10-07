dfee267335169d702ef968fb4ee85a58c70b9ceb
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-10-07 12:05:09 +0900

    C# 입출력 코딩테스트

--- C#/CodingTest/CodingTest/Program.cs
+++ C#/CodingTest/CodingTest/Program.cs
@@ -6,41 +6,11 @@ using System.Threading.Tasks;
 
 namespace CodingTest
 {
-    [System.Serializable]
-    public class C
-    {
-        public int a;
-    }
-
-    [System.Serializable]
-    public class B
-    {
-        public C c;
-    }
-
-    [System.Serializable]
-    public class A
-    {
-        public B _b;
-        public C _B
-        { get
-            {
-                return _b?.c;
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            A a = new A { _b = null };
-            C c = a._B;
-
-            if (c == null)
-            {
-                System.Console.WriteLine("null");
-            }
+            Solution_1000.Solution();
         }
     }
 }
---
+++ C#/CodingTest/CodingTest/example.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CodingTest
+{
+    class Solution_10718
+    {
+        public static void Solution()
+        {
+            System.Console.WriteLine("강한친구 대한육군\n강한친구 대한육군");
+        }
+    }
+
+    class Solution_10171
+    {
+        public static void Solution()
+        {
+            System.Console.Write(@"\    /\
+ )  ( ')
+(  /  )
+ \(__)|");
+        }
+    }
+
+    class Solution_10172
+    {
+        public static void Solution()
+        {
+            System.Console.WriteLine(@"|\_/|
+|q p|   /}
+( 0 )""""""\
+|""^""`    |
+||_/=\\__|");
+        }
+    }
+
+    class Solution_1000
+    {
+        public static void Solution()
+        {
+            var x = ReadLine().Split(); Write(int.Parse(x[0]) + int.Parse(x[1]));
+            /*
+            int A, B;
+            int.TryParse(System.Console.ReadLine(), out A);
+            int.TryParse(System.Console.ReadLine(), out B);
+            System.Console.WriteLine("{0}", A + B);
+            */
+        }
+    }
+
+}
